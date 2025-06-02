f733a3b5985cea79fd538644f3b67c37071ab505
Félix Thibault <163078989+felixthibault@users.noreply.github.com>
2025-06-02 16:10:54 -0400

    Update embarquation_b4d.rs
    Allow other file type opening and creation in binary manager.

--- src/embarquation_b4d.rs
+++ src/embarquation_b4d.rs
@@ -4,34 +4,40 @@ use std::{*, fs::File,io::*, io::prelude::*};
 use serde::Deserialize;
 
 //'! Implémentation des instances d'objets regroupés sous le nom d'une entité
-pub fn verifier_fichier(){
-    let  fichier= File::open("Préférences.json").expect("Le fichier de préférences devrait être read-only.");
-    let json: serde_json::Value = serde_json::from_reader(fichier).expect("Le fichier de préférences devrait être de format JSON");
-    //Utiliser json comme structure pour accéder au fichier JSON.
-    
-    let preference_ouverture = json.get("Ouverture_sécuritaire").unwrap();
-    let nom_par_défaut:String=
-        if preference_ouverture==true{json.get("Default_project_name").expect("Le nom par défaut du projet devrait exister
-        si l'ouverture sécuritaire est activée.").to_string()}
-        else{json.get("Preferred_default_project_name").expect("Le nom du par défaut de préférence devrait exister
-        si l'ouverture sécuritaire est désactivée.").to_string()};
-
-    let ouverture_terrain_result = File::open(&nom_par_défaut).unwrap_or_else(|error|{
+pub fn verifier_fichier(TypeDeFichier:String){
+    if TypeDeFichier=="Demarrage"{
+        //Vérification des fichiers de démarrage standarts.
+        let fichier= File::open("Préférences.json").expect("Le fichier de préférences devrait être read-only.");
+        let json: serde_json::Value = serde_json::from_reader(fichier).expect("Le fichier de préférences devrait être de format JSON");
+        //Utiliser json comme structure pour accéder au fichier JSON.
+        
+        let preference_ouverture = json.get("Ouverture_sécuritaire").unwrap();
+        let nom_par_defaut:String=
+            if preference_ouverture==true{json.get("Default_project_name").expect("Le nom par défaut du projet devrait exister
+            si l'ouverture sécuritaire est activée.").to_string()}
+            else{json.get("Preferred_default_project_name").expect("Le nom du par défaut de préférence devrait exister
+            si l'ouverture sécuritaire est désactivée.").to_string()};
+        let nom_fichier:String=nom_par_defaut;
+    }
+    else {
+        //Ouverture du fichier spécifique
+        let nom_fichier:String=TypeDeFichier;
+    }
+    let ouverture_terrain_result = File::open(&nom_fichier).unwrap_or_else(|error|{
         if error.kind() == ErrorKind::NotFound {
-            create_fichier(nom_par_défaut)
+            create_fichier(nom_fichier)
         }
         else {
             panic!("Problem opening the default file: {error:?}");
         }
     });
     print!("Fichier {ouverture_terrain_result:?} ouvert");
-    
 }
  
 fn create_fichier(nom_fichier:String)-> File{
     let mut create_fichier=File::create(&nom_fichier).expect("Problem creating the file:");
-    let message_de_création:String=format!("Voici le CAD du projet {}. Bonne chance! \n Ce document n'est pas conçu pour du DOS.",nom_fichier);
-    create_fichier.write_all(message_de_création.as_bytes()).expect("Impossible d'écrire du texte au nouveau projet.");
+    let message_de_creation:String=format!("Voici le CAD du projet {}. Bonne chance! \n Ce document n'est pas conçu pour du DOS.",nom_fichier);
+    create_fichier.write_all(message_de_creation.as_bytes()).expect("Impossible d'écrire du texte au nouveau projet.");
     return create_fichier;
 }
 
